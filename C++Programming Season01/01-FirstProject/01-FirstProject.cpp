3eab5d72ee2a6344244db540339621ce6c1289c1
gordonlee <gordon.lee.6170@gmail.com>
2020-09-16 21:04:09 -0700

    setup project

---
+++ C++Programming Season01/01-FirstProject/01-FirstProject.cpp
@@ -0,0 +1,22 @@
+// 01-FirstProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    //using std::cout;
+    //using std::endl;
+    //using std::cin;
+
+    cout << "Hello\n"; 
+    cout << "Start Leaning";
+    cout << "hello World \n";
+    
+    cin.get();
+
+    return 0;
+}
---
+++ C++Programming Season01/02-ProgrammingTest/02-ProgrammingTest.cpp
@@ -0,0 +1,46 @@
+// 02-ProgrammingTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+
+    char c = 'a';
+    char c2 = ' ';
+    char c3 = '2';
+    char c4 = '\n';
+
+    bool b1 = true;
+    bool b2 = false;
+
+    float f1 = 12.1;
+    double f2 = 12.34;
+    long double f3 = 3345430.45345;
+
+    cout << FLT_MAX << endl;
+    cout << FLT_MIN << endl;
+
+    cout << b1 << endl << b2 << endl;
+
+    3.4E9;
+
+    unsigned short e = -20;
+
+    int a = 'a';
+
+    cout << a;
+
+    cout.put('a');
+
+    char d = 97;
+
+    cout << d << endl;
+
+    int t;
+    cin >> t;
+
+    return 0;
+}
