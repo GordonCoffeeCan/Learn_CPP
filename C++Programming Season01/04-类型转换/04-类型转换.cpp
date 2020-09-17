69ddf39bc83477e2c5c89b1c5d8f4959b705e042
gordonlee <gordon.lee.6170@gmail.com>
2020-09-16 23:43:13 -0700

    new stuff learned

--- C++Programming Season01/04-类型转换/04-类型转换.cpp
+++ C++Programming Season01/04-类型转换/04-类型转换.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-    float a = 3;
+    /*float a = 3;
     int b = 4.3;
 
     float res = 4 + 4.4;
@@ -17,7 +17,16 @@ int main()
 
     int d = int(23.8);
 
-    cout << c << " + " << d << endl;
+    cout << c << " + " << d << endl;*/
+
+    auto a = 'a';
+    auto b = 100;
+    auto x = 0.0;
+
+
+
+    cout << b << endl; 
+
 
     return 0;
 
