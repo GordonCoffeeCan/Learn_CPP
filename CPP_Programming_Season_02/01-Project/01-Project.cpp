8e771696c40c8d5904fce7f4e0a47dbbc56c382b
GL <gordon.lee.6170@gmail.com>
2020-09-22 21:16:27 -0700

    functions

--- CPP_Programming_Season_02/01-Project/01-Project.cpp
+++ CPP_Programming_Season_02/01-Project/01-Project.cpp
@@ -6,8 +6,26 @@
 
 using namespace std;
 
+int myFunction(int _i);
+
 int main()
 {
+    int i = 0;
+
+    cout << "Input a value" << endl;
+    cin >> i;
+
+    cout << myFunction(i) << endl;
 
     return 0;
+}
+
+void greet() {
+    cout << "Hi guys!" << endl;
+}
+
+int myFunction(int _i) {
+    int res = _i * 10;
+
+    return res;
 }
