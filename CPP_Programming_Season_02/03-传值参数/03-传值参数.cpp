b2b9130f8f13244ca2de2ad4c29b91cd534354ff
GL <gordon.lee.6170@gmail.com>
2020-09-24 12:53:51 -0700

    new learned

---
+++ CPP_Programming_Season_02/03-传值参数/03-传值参数.cpp
@@ -0,0 +1,32 @@
+﻿// 03-传值参数.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int reset(int i);
+void resetP(int* pi);
+
+int main()
+{
+
+    int num = 10;
+
+    cout << "Reset: " << reset(num) << endl;
+    resetP(&num);
+    cout << "ResetP: " << num << endl;
+
+    return 0;
+}
+
+int reset(int i) {
+    i *= 2;
+
+    return i;
+}
+
+void resetP(int* pi) {
+    *pi *= 2;
+}
